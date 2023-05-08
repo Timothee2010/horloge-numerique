882a99f40b83bf5f500cc6833224dbc2e6ae2f31
Timothee2010 <117906444+Timothee2010@users.noreply.github.com>
2023-05-08 16:44:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ root = tk()
 root.title("clock")
 
 def time():
-  string = strftime("%H:%M:%S %p")
+  string = strftime("%I:%M:%S %p")
   label.config(text=string)
   label.after(1000, time)
-  label = label(root
+  
+label = label(root, font=("ds-digital", 80), background="black", foreground="cyan")
+label.pack(anchor="center")
+time()
+
+mainloop()
